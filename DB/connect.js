5491a0db492bbb251a2b37b7ce44ace09469b80f
collins <collinsolayemi@gmail.com>
2023-01-12 03:36:28 -0800

    YES

--- DB/connect.js
+++ DB/connect.js
@@ -1,12 +1,9 @@
 const mongoose = require("mongoose");
 
-const connectionStr =
-  "mongodb+srv://collinsolayemi1995:faVZjguOY5LMJ3Ih@cluster0.emmrqq8.mongodb.net/test?retryWrites=true&w=majority";
-
 const connectDB = (url) => {
   return mongoose
     .set("strictQuery", false)
-    .connect(connectionStr, {
+    .connect(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
--- app.js
+++ app.js
@@ -2,15 +2,25 @@ const express = require("express");
 const app = express();
 const task = require("./routes/tasks-routes");
 const connectDB = require("./DB/connect");
-
+require("dotenv").config();
 //middleware
 app.use(express.json());
 
 app.use("/api/v1/tasks", task);
 
 const port = 5500;
-app.listen(port, () => {
-  {
-    console.log(`server running on port ${port}`);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      {
+        console.log(`server running on port ${port}`);
+      }
+    });
+  } catch (error) {
+    console.log(error);
   }
-});
+};
+
+start();
