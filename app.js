9e0db250f6e48dab9ec63057e14a587aa39003d1
collins <collinsolayemi@gmail.com>
2023-01-11 18:32:03 -0800

    changes

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const taskRoute = require("./routes/tasks-routes");
+
+// app.get("/api/v1/task", (req, res) => {
+//   res.send("task manager running");
+// });
+
+app.use("/api/v1/tasks", taskRoute);
+
+const port = 5500;
+app.listen(port, () => {
+  {
+    console.log(`server running on port ${port}`);
+  }
+});
