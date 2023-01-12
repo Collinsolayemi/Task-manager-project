8addeddd3131ad8e71d131d37323a78153bf369e
collins <collinsolayemi@gmail.com>
2023-01-11 19:26:55 -0800

    ss

--- controller/task-controller.js
+++ controller/task-controller.js
@@ -6,7 +6,22 @@ const createTask = (req, res) => {
   res.send("Task created succesfully");
 };
 
+const getTask = (req, res) => {
+  res.send("task gotten");
+};
+
+const updateTask = (req, res) => {
+  res.send("Task updated succesfully");
+};
+
+const deleteTask = (req, res) => {
+  res.send("Task deleted succesfully");
+};
+
 module.exports = {
   getAllTasks,
   createTask,
+  getTask,
+  updateTask,
+  deleteTask,
 };
--- routes/tasks-routes.js
+++ routes/tasks-routes.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { getAllTasks, createTask } = require("../controller/task-controller");
+const {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+} = require("../controller/task-controller");
 
 router.route("/").get(getAllTasks).post(createTask);
+router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
 
 module.exports = router;
