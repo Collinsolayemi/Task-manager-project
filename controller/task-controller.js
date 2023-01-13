e5016596cf5d00e4825abfee4c7eb588cbfd7a14
collins <collinsolayemi@gmail.com>
2023-01-12 18:32:48 -0800

    new

--- controller/task-controller.js
+++ controller/task-controller.js
@@ -66,7 +66,13 @@ const getTask = async (req, res) => {
 const updateTask = async (req, res) => {
   try {
     const { id: taskId } = req.params;
-    const task = await Task.updateOne({ _id: taskId });
+    const task = await Task.updateOne({ _id: taskId }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskId}` });
+    }
     res.status(200).json({
       status: "successful",
       data: {
